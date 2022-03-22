7e8b4bd33a35ad324afea36e0f09ba66190448ad
Jack Logan <jackmlogan92@gmail.com>
2022-03-22 11:44:10 +0000

    Add passport authentication

--- routes/auth_login.js
+++ routes/auth_login.js
@@ -4,14 +4,35 @@ const path = require('path');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const crypto = require('crypto');
-// const db = require('../db');
+const db = require('../db/db');
 
+// Configure Passport localStrategy authentication
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  db.get('SELECT rowid AS id, * FROM customers WHERE username = ?', [username], function(err, row) {
+    if (err) { return cb(err) }
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' })}
 
-// Login GET route
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err) }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.'});
+      }
+      return cb(null, row);
+    })
+  })
+}))
+
+// GET log in page
 loginRouter.get('/', (request, response) => {
   response.sendFile(path.resolve('./public/auth.html'));
   response.status(200)
 });
+
+// POST form submission. NOT YET WORKING. CHECK route/failure-/success-routes. Check username vs req.body.username?
+loginRouter.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/auth'
+}));
 
 // // Login POST route: accept user credentials
 // loginRouter.post('/login', (req, res) => {
