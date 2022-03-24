77d604d2dc75256c6856b9427136c9e97e874b85
Jack Logan <jackmlogan92@gmail.com>
2022-03-24 12:13:42 +0000

    Work on Passport localStrategy

--- routes/auth_login.js
+++ routes/auth_login.js
@@ -6,14 +6,21 @@ const LocalStrategy = require('passport-local');
 const crypto = require('crypto');
 const db = require('../db/db');
 
-// Configure Passport localStrategy authentication
+// Configure Passport LocalStrategy 
 passport.use(new LocalStrategy(function verify(username, password, cb) {
-  db.get('SELECT rowid AS id, * FROM customers WHERE username = ?', [username], function(err, row) {
-    if (err) { return cb(err) }
-    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' })}
+  // changing ? parameter to $1 causes app to crash
+  db.query('SELECT * FROM customers WHERE username = ?', [username], function(err, row) {
+    if (err) { 
+      return cb(err) 
+    }
+    if (username.rowCount === 0) { 
+      return cb(null, false, { message: 'Incorrect username or password.' })
+    }
 
     crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err) }
+      if (err) { 
+        return cb(err) 
+      }
       if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
         return cb(null, false, { message: 'Incorrect username or password.'});
       }
@@ -28,18 +35,11 @@ loginRouter.get('/', (request, response) => {
   response.status(200)
 });
 
-// POST form submission. NOT YET WORKING. CHECK route/failure-/success-routes. Check username vs req.body.username?
-loginRouter.post('/auth/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/auth'
+// POST form submission w/passport authentication
+loginRouter.post('/', passport.authenticate('local', {
+  successRedirect: '../public/index.html',
+  failureRedirect: '/'
 }));
 
-// // Login POST route: accept user credentials
-// loginRouter.post('/login', (req, res) => {
-//     // Insert Login Code Here
-//     let username = req.body.username;
-//     let password = req.body.password;
-//     res.send(`Username: ${username} Password: ${password}`);
-//   });
 
-  module.exports = loginRouter;
+module.exports = loginRouter;
